b90f4e4896ca40af1056cc265a1a452e78d7de1b
Ovidiu Moldovan <ovi1602@gmail.com>
2020-03-01 23:16:44 +0200

    first swiftUI view

---
+++ watchConnectivitySample/watchConnectivitySample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchConnectivitySample
+//
+//  Created by Ovidiu Moldovan on 01/03/2020.
+//  Copyright © 2020 Ovidiu Moldovan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
