dcddb8d93cd17dab2152352194b79b7f8c105fd3
Ovidiu Moldovan <ovi1602@gmail.com>
2020-06-15 20:36:08 +0300

    Form validations

--- watchInfoWebApp/watchInfoWebApp/ClientApp/src/components/PostItemData.js
+++ watchInfoWebApp/watchInfoWebApp/ClientApp/src/components/PostItemData.js
@@ -5,14 +5,26 @@ export class PostItemData extends Component {
   static displayName = "PostItemData";
   constructor(props) {
     super(props);
-    this.state = { heartBpm: 0 };
+    this.state = { heartBpm: 0, errors: {}, validForm: false, status: "" };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     const { name, value } = e.target;
-    this.setState({ [name]: value });
+    this.setState((state) => {
+      state[name] = value;
+
+      if (state.heartBpm == "" || state.heartBpm == 0) {
+        state.errors["name"] = "If you're sending BPMs, you're probably alive.";
+        state.validForm = false;
+      } else {
+        state.errors["name"] = undefined;
+        state.validForm = true;
+      }
+      state.status = "";
+      return state;
+    });
   }
 
   handleSubmit(event) {
@@ -32,19 +44,25 @@ export class PostItemData extends Component {
 
     console.log(requestOptions);
 
+    this.setState((state) => {
+      state.validForm = false;
+      state.status = "Sent.";
+      return state;
+    });
+
     return fetch(`Api/DataItem`, requestOptions);
   }
 
   render() {
     console.log(authHeader().token);
-    const { heartBpm } = this.state;
+    const { heartBpm, errors, status } = this.state;
     return (
       <div>
         <h1>Send BPM data</h1>
 
         <form name="form" onSubmit={this.handleSubmit}>
           <input
-            type="text"
+            type="number"
             className="form-control"
             name="heartBpm"
             value={heartBpm}
@@ -53,10 +71,13 @@ export class PostItemData extends Component {
           <input
             type="submit"
             onClick={this.handleSubmit}
-            value="Login"
+            value="Send"
             className="btn btn-primary"
+            disabled={!this.state.validForm}
           />
         </form>
+        <span style={{ color: "red" }}>{errors["name"]}</span>
+        <span style={{ color: "green" }}> {status}</span>
       </div>
     );
   }
