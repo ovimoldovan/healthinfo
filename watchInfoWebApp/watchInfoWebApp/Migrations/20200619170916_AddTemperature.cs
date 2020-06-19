23d0a8c1da8dd9540bceaeac9552e71641f49beb
Ovidiu Moldovan <ovi1602@gmail.com>
2020-06-19 20:29:30 +0300

    Temperature migration

---
+++ watchInfoWebApp/watchInfoWebApp/Migrations/20200619170916_AddTemperature.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace watchInfoWebApp.Migrations
+{
+    public partial class AddTemperature : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Temperature",
+                table: "DataItems",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Temperature",
+                table: "DataItems");
+        }
+    }
+}
